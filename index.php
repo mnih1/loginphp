21bae8282060947bae2897d90c4826ec476c2858
mnih1 <mnih550@gmail.com>
2022-12-07 11:49:40 +0100

    New changes

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="./assets/css/all.css">
+    <link rel="stylesheet" href="./assets/css/sass/main.css">
+    
+    <title>Stona logowania do CV</title>
+</head>
+<body>
+    
+
+
+</body>
+</html>
