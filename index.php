b7cd60f6abd7d9a075dc05deb1397ea3b6f723cf
mnih1 <mnih550@gmail.com>
2022-12-15 16:13:18 +0100

    new things - footer has been added

--- index.php
+++ index.php
@@ -44,6 +44,26 @@
         </div>
     </div>
 
+    <footer>
+        <div class="main">
+            <div class="box">
+                
+            </div>
+            <div class="box">
+
+            </div>
+            <div class="box">
+
+            </div>
+            <div class="box">
+
+            </div>
+        </div>
+        <div class="copyright">
+            2022 &copy; MNIH
+        </div>
+    </footer>
+
 
 
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
