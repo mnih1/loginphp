24b947fdacbbd23841eb95cbed82eaf2e7aae42a
mnih1 <mnih550@gmail.com>
2022-12-16 15:27:58 +0100

    SASS doesnt work - IDK why ---> I will repair it

--- zaloguj.php
+++ zaloguj.php
@@ -50,10 +50,9 @@
         </form>
     </div>
 
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script>
     AOS.init();
 </script>
-
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 </body>
 </html>
